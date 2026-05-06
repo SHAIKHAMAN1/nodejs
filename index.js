51ad01b3b71c3b92e6185b6d9fea790e6d773a0d
Shaikh Aman <amaanahmed2022@gmail.com>
2026-05-06 16:52:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/',(req,res)=>{
-    return res.json({msg:'Hello From Server'})
+    return res.json({msg:'Hello From Server kaisa hai bhaiiiiii'})
 })
 
 app.listen(PORT,()=>{
